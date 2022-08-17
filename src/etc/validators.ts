b486f99af0eb1aba55fd629e7d0a32b98c00e37a
Joshua <joshua+github@frontlawn.net>
2022-08-16 23:16:05 -0700

    refactor: Update validators.

--- src/etc/validators.ts
+++ src/etc/validators.ts
@@ -1,21 +1,26 @@
-import createValidator from '@darkobits/valida';
+import createValidator, { type Ow } from '@darkobits/valida';
 
+const cosmiconfigOptionsSpec = (ow: Ow) => ow.object.partialShape({
+  auto: ow.optional.boolean,
+  searchFrom: ow.optional.string,
+  fileName: ow.string.nonEmpty,
+  key: ow.optional.string
+});
 
 export default {
   cosmiconfigOptions: createValidator(({ ow }) => ({
-    spec: ow.object.partialShape({
-      searchFrom: ow.optional.string,
-      fileName: ow.string.nonEmpty,
-      key: ow.optional.string
-    })
+    spec: cosmiconfigOptionsSpec(ow)
   })),
   saffronCommand: createValidator(({ ow }) => ({
     spec: {
-      command: ow.optional.string,
-      description: ow.optional.string,
-      strict: ow.optional.boolean,
-      config: ow.any(ow.boolean.false, ow.object, ow.undefined),
+      command: ow.optional.string.nonEmpty,
+      description: ow.optional.string.nonEmpty,
       aliases: ow.any(ow.string, ow.array.ofType(ow.string), ow.undefined),
+      strict: ow.optional.boolean,
+      config: ow.optional.any(
+        ow.boolean.false,
+        cosmiconfigOptionsSpec(ow)
+      ),
       builder: ow.optional.function,
       handler: ow.function
     }
