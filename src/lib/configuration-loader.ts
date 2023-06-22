ce0db75c2963fbe221ef94cd62914557b41b3d61
Joshua <joshua+github@frontlawn.net>
2023-06-22 12:59:47 -0700

    chore: Remove cache directory created by `ts-import`.

--- src/lib/configuration-loader.ts
+++ src/lib/configuration-loader.ts
@@ -147,15 +147,17 @@ async function withTsImport(filePath: string) {
     // Slower, but allows files to be considered as part of larger TypeScript
     // programs, which should allow path mappings to work.
     mode: tsImport.LoadMode.Compile,
+    // N.B. Even with this set to false, ts-import still seems to create (and
+    // leave behind) a .cache directory.
     useCache: false
   });
 
   // Clean-up the cache directory left behind by ts-import.
-  // try {
-  //   await fs.remove(path.join(path.dirname(filePath), '.cache'));
-  // } catch (err: any) {
-  //   log.error(log.prefix('ts-import'), 'Error removing cache directory:', err);
-  // }
+  try {
+    await fs.remove(path.join(path.dirname(filePath), '.cache'));
+  } catch (err: any) {
+    log.error(log.prefix('ts-import'), 'Error removing cache directory:', err);
+  }
 
   return result?.default || result;
 }
