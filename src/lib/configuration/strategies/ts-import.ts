13c9c3fb67450b2996ad68d864fefd701ecffab2
Joshua <joshua+github@frontlawn.net>
2023-06-25 01:30:39 -0700

    fix: `ts-import` strategy removes cache directory.

--- src/lib/configuration/strategies/ts-import.ts
+++ src/lib/configuration/strategies/ts-import.ts
@@ -10,21 +10,23 @@ import log from 'lib/log';
  * Uses ts-import to dynamically import TypeScript configuration files.
  */
 export async function tsImportStrategy(filePath: string) {
-  const result = await tsImport.load(filePath, {
-    // Slower, but allows files to be considered as part of larger TypeScript
-    // programs, which should allow path mappings to work.
-    mode: tsImport.LoadMode.Compile,
-    // N.B. Even with this set to false, ts-import still seems to create (and
-    // leave behind) a .cache directory.
-    useCache: false
-  });
-
   // Clean-up the cache directory left behind by ts-import.
+  const cacheDir = path.join(path.dirname(filePath), '.cache');
+
   try {
-    await fs.remove(path.join(path.dirname(filePath), '.cache'));
+    const result = await tsImport.load(filePath, {
+      // Slower, but allows files to be considered as part of larger TypeScript
+      // programs, which should allow path mappings to work.
+      mode: tsImport.LoadMode.Compile,
+      // N.B. Even with this set to false, ts-import still seems to create (and
+      // leave behind) a .cache directory.
+      useCache: false
+    });
+
+    return result?.default || result;
   } catch (err: any) {
     log.error(log.prefix('ts-import'), 'Error removing cache directory:', err);
+  } finally {
+    if (await fs.exists(cacheDir)) await fs.remove(cacheDir);
   }
-
-  return result?.default || result;
 }
