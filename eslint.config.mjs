0ddfef8850b4b4c9a1c8cfa43a63f521bd9dc642
Joshua <joshua+github@frontlawn.net>
2023-07-08 13:22:13 -0700

    chore: Update linting rules.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,10 @@ export default [
   {
     rules: {
       'no-console': 'off',
-      'unicorn/no-process-exit': 'off'
+      'require-atomic-updates': 'off',
+      'unicorn/no-process-exit': 'off',
+      // TODO: Disable this rule in eslint-plugin.
+      '@typescript-eslint/prefer-nullish-coalescing': 'off'
     }
   }
 ];
